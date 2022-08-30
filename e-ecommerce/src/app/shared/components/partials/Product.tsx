cc9b90cac5acc219e33f74344b850a1c182fd03e
Phan Huu Lan <lanhuu0603@gmail.com>
2022-08-30 12:44:46 +0700

    router react

--- e-ecommerce/src/app/shared/components/partials/Product.tsx
+++ e-ecommerce/src/app/shared/components/partials/Product.tsx
@@ -1,11 +1,12 @@
 
-import React, { useState } from "react";
-import { useSelector } from "react-redux";
+import React from "react";
+//import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 import { IProduct } from "../../../interface/product-interface";
-import { IRootState } from "../../../interface/state-interface";
+//import { IRootState } from "../../../interface/state-interface";
 
 const Product : React.FC = (props) => {
-    const products = useSelector((state : IRootState ) => state.allProducts.products);
+    //const products = useSelector((state : IRootState ) => state.allProducts.products);
 
 
 
@@ -19,19 +20,21 @@ const Product : React.FC = (props) => {
       
       return (
         <li className="product-item col-3" key={id}>
-          <div className="card">
-            <a href="" className="card-link">
-              <div className="image-product">
-                <img src={image} alt={title} className="image" />
-              </div>
-              <div className="card-content">
-                <h4 className="card-title">{title.toUpperCase()}</h4>
-                <span className="card-price">{price}$</span>
-                <p className="card-category">{category}</p>
-              </div>
-              {/* <button className="btn btn-addToCart">ADD TOO CART</button> */}
-            </a>
-          </div>
+          <Link to={`/product.${id}`}>     
+            <div className="card">
+              <a href="" className="card-link">
+                <div className="image-product">
+                  <img src={image} alt={title} className="image" />
+                </div>
+                <div className="card-content">
+                  <h4 className="card-title">{title.toUpperCase()}</h4>
+                  <span className="card-price">{price}$</span>
+                  <p className="card-category">{category}</p>
+                </div>
+                {/* <button className="btn btn-addToCart">ADD TOO CART</button> */}
+              </a>
+            </div>
+          </Link>
         </li>
       )
     })
--- e-ecommerce/src/app/shared/components/partials/ProductList.tsx
+++ e-ecommerce/src/app/shared/components/partials/ProductList.tsx
@@ -1,59 +1,65 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { apiProducts, categoryProduct } from "../../../apis/data";
+import { apiProducts } from "../../../apis/data";
 import { setProducts } from "../../../containers/redux/actions/productAction";
-import { IProduct } from "../../../interface/product-interface";
 import { IRootState } from "../../../interface/state-interface";
 import Product from "./Product";
 
-
-const ProductList : React.FC = () => {
-  
-  const products = useSelector((state : IRootState ) => state.allProducts.products);
+const ProductList: React.FC = () => {
+  const products = useSelector(
+    (state: IRootState) => state.allProducts.products
+  );
   const dispatch = useDispatch();
 
   const fetchProducts = async () => {
-    const response : any = await apiProducts()
-    .catch((err) => {
-      console.log("ERROR",err);  
-    })
-    dispatch(setProducts(response.data) );
-  }
+    const response: any = await apiProducts().catch((err) => {
+      console.log("ERROR", err);
+    });
+    dispatch(setProducts(response.data));
+  };
 
   useEffect(() => {
-    fetchProducts()
-  },[products]);
-  
-  const[data, setData] = useState(products);
-  const filterResult = (catItem : any) => {
-    const result = products.filter((curDate : any) => {
-        return curDate.category === catItem;
+    fetchProducts();
+  }, [products]);
+
+  const [data, setData] = useState(products);
+  const filterResult = (catItem: any) => {
+    const result = products.filter((curDate: any) => {
+      return curDate.category === catItem;
     });
     setData(result);
+  };
+
+  if (data.length === 0) {
+    data.push(...products);
   }
-  
-  if(data.length === 0) {  
-    data.push(...products)
-  }
-  
+  const handleCategoryChange = (event: any) => {
+    filterResult(event.target.value);
+  };
   return (
     <section className="section-collection">
       <div className="container">
-      <h3 className="collection-title">Latest Products Collection</h3>
-      <div className="row">
-              <div className="col">
-                  <button className='btn' onClick={() => setData(products)}>All</button>
-                  <button className='btn' onClick={() => filterResult("electronics")}>electronics</button>
-                  <button className='btn' onClick={() => filterResult("jewelery")}>jewelery</button>
-                  <button className='btn' onClick={() => filterResult("men's clothing")}>men's clothing</button>
-                  <button className='btn' onClick={() => filterResult( "women's clothing")}>women's clothing</button>
-              </div>
-      </div>
-      <ul className="product-list row ">
-        <Product {...data} />
-      </ul>
+        <div className="collection-header">
+          <h3 className="collection-title">Latest Products Collection</h3>
+          <div className="category-product">
+            <select
+              name="category-list"
+              id="category-list"
+              onChange={handleCategoryChange}
+            >
+              <option value="">All</option>
+              <option value="electronics">Electronics</option>
+              <option value="jewelery">Jewelery</option>
+              <option value="men's clothing">Men's clothing</option>
+              <option value="women's clothing">Women's clothing</option>
+            </select>
+          </div>
+        </div>
+        <ul className="product-list row ">
+          <Product {...data} />
+        </ul>
       </div>
     </section>
-  )
-}
+  );
+};
 export default ProductList;
